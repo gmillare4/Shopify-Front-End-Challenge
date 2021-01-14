c9b96add290ac580ee25622883b37e09e1b9c022
Gregory Millare <gmillare4@gmail.com>
2021-01-14 17:16:53 -0600

    search bar can fetch movies from ombd's api based on movie title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { Nominations, SearchBar, SearchResults } from "./components";
 export default class App extends Component {
   constructor() {
     super();
+    this.state = {
+      searchParam: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.search = this.search.bind(this);
   }
   //http://www.omdbapi.com/?i=tt3896198&apikey=807abc94
   //http://www.omdbapi.com/?apikey=807abc94
@@ -15,12 +20,30 @@ export default class App extends Component {
       .then((data) => console.log("Data", data));
   }
 
+  search(event) {
+    event.preventDefault();
+    console.log("Search Parameter", this.state.searchParam);
+    fetch(
+      `http://www.omdbapi.com/?s=${this.state.searchParam}&page=1&apikey=807abc94`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log("Data", data));
+  }
+
+  handleChange = (event) => {
+    this.setState({ searchParam: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Something</h1>
         <Nominations />
-        <SearchBar />
+        <SearchBar
+          search={this.search}
+          searchparam={this.state.searchParam}
+          handleChange={this.handleChange}
+        />
         <SearchResults />
       </div>
     );
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
       <h2>SearchBar</h2>
+      <form onSubmit={props.search}>
+        <input
+          type="text"
+          value={props.searchParam}
+          onChange={props.handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
